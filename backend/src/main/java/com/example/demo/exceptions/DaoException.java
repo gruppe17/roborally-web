f0c40d74941d49fd849d39f91d3004e07aeeffea
Tobias Nyholm Maneschijn <tfrom222@gmail.com>
2021-05-05 20:20:45 +0200

    Moved backend to new repo

--- backend/src/main/java/com/example/demo/exceptions/DaoException.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.exceptions;
-
-public class DaoException extends Exception{
-//TODO: Figure out what fields the exception should contain
-}
--- backend/src/main/java/com/example/demo/exceptions/MappingException.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.exceptions;
-
-public class MappingException extends Exception{
-
-    public MappingException(String message) {
-        super(message);
-    }
-}
