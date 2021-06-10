925c5ade1cb710b89d17b6c704645a12f808179e
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2021-06-10 13:15:02 +0200

    Games are now buttons

--- frontend/src/components/menu-tabs/GamesListTabComponent.tsx
+++ frontend/src/components/menu-tabs/GamesListTabComponent.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@material-ui/core";
+import { Box, Button, Typography } from "@material-ui/core";
 import * as React from "react";
 import { useEffect, useState } from "react";
 import GameApi from "../../api/GameApi";
@@ -11,9 +11,9 @@ function GameComponent(props : {game: Board}) {
   return (
     <Box height="64px" justifyContent="start"
      alignItems="center" bgcolor="#404040" display="flex" flexDirection="row" width="100%">
-      <Typography>
+      <Button variant="contained" color="primary" onClick={() => {}}>
         Game: "{props.game.boardName }" Players: { props.game.playerDtos && props.game.playerDtos.length}
-      </Typography>
+      </Button>
     </Box>
   );
 }
