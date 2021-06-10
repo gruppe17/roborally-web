8a1defd050cfaeac66ae225d01d5d964c7bfae6d
Tobias Nyholm Maneschijn <tfrom222@gmail.com>
2021-06-10 14:51:28 +0200

    test

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { unmountComponentAtNode } from 'react-dom';
+import { JsxElement } from 'typescript';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+
+
+
+/*
+@author tobiasmaneschijn
+*/
+test('renders board', async () => {
+  const app = render(<App />);
+  const board = await app.findByTestId('board')
+  expect(board).toBeInTheDocument();
 });
