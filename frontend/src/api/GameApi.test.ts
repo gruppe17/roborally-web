15f23f45b94f6bf27468617eb3fa04d6893e07ac
Tobias Nyholm Maneschijn <tfrom222@gmail.com>
2021-06-11 14:01:29 +0200

    get games test is up

--- frontend/src/api/GameApi.test.ts
+++ frontend/src/api/GameApi.test.ts
@@ -2,7 +2,7 @@ import GameApi from "./GameApi"
 import { Game } from '../types/Game';
 
 
-describe('creating and removing games games', () => {
+describe('creating and removing games', () => {
     let game: Game;
     test('should create a game and receive it back', async () => {
         const newGame = await GameApi.createGame();
@@ -17,12 +17,9 @@ describe('creating and removing games games', () => {
     test('should remove a game and receive a success', async () => {
         const result = await GameApi.removeGame(game.gameId)
         expect(result).toBe(true);
-        
     })
 
-
 })
-
 
 
 
@@ -36,8 +33,20 @@ describe('Games', () => {
     test('should get games from backend', async () => {
         // getGames has already been tested in the previous test.
         const result = await GameApi.getGames()
+        let foundGame = false;
+        result.data.forEach(resultGame => {
+            if (resultGame.gameId === game.gameId) {
+                foundGame = true;
+            }
+            
+        });
+        expect(foundGame).toBe(true);
 
     })
+
+    
+
+
     afterEach(() => {
         GameApi.removeGame(game.gameId);
     })
