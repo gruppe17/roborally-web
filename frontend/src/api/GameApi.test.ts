b3f9574faaa8e21bc7818e0e3a7f2871a2830fde
Tobias Nyholm Maneschijn <tfrom222@gmail.com>
2021-06-11 13:21:21 +0200

    First tests

--- frontend/src/api/GameApi.test.ts
+++ frontend/src/api/GameApi.test.ts
@@ -1,12 +1,36 @@
 import GameApi from "./GameApi"
+import { Game } from '../types/Game';
+
+
+describe('creating and removing games games', () => {
+    test('should create a game and receive it back', async () => {
+        const newGame = await GameApi.createGame();
+
+        expect(newGame).not.toBeNull()
+        expect(newGame.gameId).not.toBeNull()
+        expect(newGame.started).toBeFalsy()
+        expect(newGame.users).toBeNull();
+        expect(newGame.name).toBeNull()
+    })
 
 
 
+})
 
 
 describe('Games', () => {
-    
-    test('should get games from backend', () => {
+
+    let game: Game
+    beforeEach(async () => {
+        game = await GameApi.createGame();
     })
-    
+
+    test('should get games from backend', async () => {
+        // getGames has already been tested in the previous test.
+        const result = await GameApi.getGames()
+        
+    })
+    afterEach(() => {
+        GameApi.removeGame(game.gameId);
+    })
 })
