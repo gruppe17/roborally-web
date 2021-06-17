9b7085ea22ef17a2ff818ed89e725cbfe2f223e4
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2021-06-17 10:46:48 +0200

    Changed back to localhost

--- frontend/src/api/GameApi.ts
+++ frontend/src/api/GameApi.ts
@@ -9,7 +9,7 @@ class GameApi {
     
 
     private static instance: GameApi;
-    private readonly BACKEND_URL = process.env.BACKEND_URL //   "https://roborallyserver.tobiasmaneschijn.com/"
+    private readonly BACKEND_URL = "http://localhost:8080" // process.env.BACKEND_URL //   "https://roborallyserver.tobiasmaneschijn.com/"
     private constructor() { }
 
     public static getInstance(): GameApi {
