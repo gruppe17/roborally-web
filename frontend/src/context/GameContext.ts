87f23dc11ff8bd3ce4da5900d22127bcf7654c74
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2021-06-17 16:52:16 +0200

    Forgot to commit GameContext

--- frontend/src/context/GameContext.ts
+++ frontend/src/context/GameContext.ts
@@ -2,24 +2,23 @@ import {createContext} from "react";
 import {Board} from "../types/Board";
 import { Game, NO_GAME_GAMEID } from "../types/Game";
 import {Space} from "../types/Space";
-import { User } from "../types/User";
+
 
 export type GameContextType = {
     // Information about games
     games: Game[]
     selectGame: (gameId: number) => Promise<void>,
     unselectGame: () => Promise<void>,
-    currentUser : User,
-    currentGame : Game,
+    createGame : () => Promise<number>,
     deleteGame :  (gameId: number) => Promise<void>,
-    loaded : boolean,
+    startGame : (gameId: number) => Promise<Boolean>,
+    changeGameName : (gameId: number, name: string) => Promise<boolean>,
+    currentGame : Game,
     board: Board,
+    loaded : boolean,
     setCurrentPlayerOnSpace: (space: Space) => Promise<void>,
     switchCurrentPlayer: () => Promise<void>,
     forceViewUpdate : () => void,
-    createGame : () => Promise<number>,
-    startGame : (gameId: number) => Promise<Boolean>,
-    changeGameName : (gameId: number, name: string) => Promise<boolean>,
 }
 //Define a new context of type GameContextType
 //Below we define the "default" values which are set upon initialization of the context
@@ -31,12 +30,6 @@ const GameContext = createContext<GameContextType>({
         name: "No game loaded",
         started: false,
         users: []
-
-    },
-    currentUser: {
-        userId: 0,
-        userName: "Not logged in!",
-        currentGame: NO_GAME_GAMEID
     },
     selectGame: async () => {},
     unselectGame: async () => {},
